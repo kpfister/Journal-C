e568eff9044f7b4f00ec08bfd70a9b3743f29f26
kpfister <kpfister@me.com>
2016-05-31 16:49:41 -0600

    Master List View
    Completed the Master List View section. With Prepare for segue

---
+++ Journal - C/Journal - C/EntryListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EntryListTableViewController.h
+//  Journal - C
+//
+//  Created by Karl Pfister on 5/31/16.
+//  Copyright © 2016 Karl Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryListTableViewController : UITableViewController
+
+@end
